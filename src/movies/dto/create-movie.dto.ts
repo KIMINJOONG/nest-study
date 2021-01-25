85789c4c5e03ee3bdd93e5e6df41a6c4148514e6
KIMINJOONG <ijboym216@gmail.com>
2021-01-25 19:47:09 +0900

    fix

--- src/movies/dto/create-movie.dto.ts
+++ src/movies/dto/create-movie.dto.ts
@@ -1,12 +1,12 @@
-import { IsNumber, IsString } from "class-validator";
+import { IsNumber, IsString } from 'class-validator';
 
 export class CreateMovieDto {
-    @IsString()
-    readonly title: string;
-    
-    @IsNumber()
-    readonly year: number;
+  @IsString()
+  readonly title: string;
 
-    @IsString({each: true})
-    readonly genres: string[];
+  @IsNumber()
+  readonly year: number;
+
+  @IsString({ each: true })
+  readonly genres: string[];
 }
